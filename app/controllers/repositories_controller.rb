0ee8f27d1f51d3fc9ce117a8cad8aac9ed1f8643
Elle Wilson <eleanorwilsonn@gmail.com>
2018-09-11 17:21:23 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
     @resp = Faraday.get 'https://developer.github.com/v3/search/repositories' do |req|
         req.params['client_id'] = '11TR0GATVWADKTY34TC2ZOWPM1V3O1XMRG3OIJ3B55XHVK2K'
         req.params['client_secret'] = 'RKLE5QI2ZE3K2GOYJIRTT5XOTOA0FEALIPJQ3H4G0PBVUXGF'
